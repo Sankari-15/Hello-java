af0553285450965c8fbf02e65480c10b92618907
Sankari <152249607+Sankari-15@users.noreply.github.com>
2025-07-12 17:04:50 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Sankari!");
+        System.out.println("Hello, Sankari! CI/CD is working!");
+
     }
 }
